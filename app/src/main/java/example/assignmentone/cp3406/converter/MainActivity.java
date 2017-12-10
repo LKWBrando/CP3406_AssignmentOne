5550ec805d0e1e948121ccfef632fa26a34e0bc8
LKWBrando <brandon.lum@my.jcu.edu.au>
2017-12-11 00:00:45 +0800

    Added layout structure for the currency activity, pending functionality additions

--- app/src/main/java/example/assignmentone/cp3406/converter/MainActivity.java
+++ app/src/main/java/example/assignmentone/cp3406/converter/MainActivity.java
@@ -1,13 +1,39 @@
 package example.assignmentone.cp3406.converter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton currencyButton;
+    ImageButton unitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currencyButton = findViewById(R.id.currencyButton);
+        unitButton = findViewById(R.id.unitButton);
     }
+
+    public void onButtonPress(View view){
+        switch(view.getId()){
+            case R.id.currencyButton:
+                Intent convertCurrency = new Intent(this, CurrencyActivity.class);
+                startActivity(convertCurrency);
+                break;
+
+            case R.id.unitButton:
+                Intent convertUnits = new Intent(this, UnitsActivity.class);
+                startActivity(convertUnits);
+                break;
+        }
+
+    }
+
+
 }
