f64b0e8126f69113aaaa0919469a0cd69a36817e
LKWBrando <brandon.lum@my.jcu.edu.au>
2017-12-12 00:57:01 +0800

    Basic functionality of the currency converter achieved, including most error checks (pending further testing).

--- app/src/main/java/example/assignmentone/cp3406/converter/ConvertCurrency.java
+++ app/src/main/java/example/assignmentone/cp3406/converter/ConvertCurrency.java
@@ -16,8 +16,9 @@ public class ConvertCurrency extends AppCompatActivity {
     EditText changedCurrency;
     TextView initialCurrencyString;
     TextView changedCurrencyString;
-    int initialCurrencyFactor;
-    int changedCurrencyFactor;
+    double userInput;
+    double userInputUsd;
+    double convertedResult;
     SharedPreferences preferences;
 
     @Override
@@ -32,46 +33,14 @@ public class ConvertCurrency extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
+    protected void onStart(){
         super.onStart();
+        converterMain();
+    }
 
-        //initialCurrencyFactor = preferences.getInt()
-        //changedCurrencyFactor = preferences.getInt()
-
-        initialCurrency.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                try{
-                    int value = Integer.parseInt(charSequence.toString());
-                }
-                catch(Exception e){}
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-            }
-        });
-
-        changedCurrency.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
+    protected void onResume(){
+        super.onResume();
+        converterMain();
     }
 
     public void onButtonPress(View view){
@@ -83,13 +52,158 @@ public class ConvertCurrency extends AppCompatActivity {
                 break;
 
             case R.id.initialCurrencyButton:
+                preferences.edit().putBoolean("currencyBoolean", true).apply();
                 startActivity(goToCurrencyMenu);
                 break;
 
             case R.id.changedCurrencyButton:
+                preferences.edit().putBoolean("currencyBoolean", false).apply();
                 startActivity(goToCurrencyMenu);
                 break;
 
+            case R.id.resetButton:
+                initialCurrencyString.setText("Choose a currency!");
+                changedCurrencyString.setText("Choose a currency!");
+                preferences.edit().putString("Option1", null).apply();
+                preferences.edit().putString("Option2", null).apply();
+                initialCurrency.setText("");
+                changedCurrency.setText("");
+                userInput = 0;
+                userInputUsd = 0;
+                convertedResult = 0;
         }
     }
+
+   public void converterMain(){
+
+       initialCurrencyString.setText(preferences.getString("Option1", "Choose a currency!"));
+       changedCurrencyString.setText(preferences.getString("Option2", "Choose a currency!"));
+       initialCurrency.setText(preferences.getString("initialUserInput", null));
+
+       initialCurrency.addTextChangedListener(new TextWatcher() {
+           @Override
+           public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+           }
+
+           @Override
+           public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+               try{
+                   preferences.edit().putString("initialUserInput", charSequence.toString()).apply(); //to save the value when returning from choosing currency type
+                   userInput = Double.parseDouble(charSequence.toString());
+                   if(!initialCurrencyString.getText().toString().equals("Choose a currency!")  && !changedCurrencyString.getText().toString().equals("Choose a currency!")){
+                       convertUSD(initialCurrencyString.getText().toString(), userInput);
+                       convertActual(changedCurrencyString.getText().toString(), userInputUsd);
+                       changedCurrency.setText(Double.toString(convertedResult));
+                   }
+               }
+               catch(Exception e){}
+           }
+
+           @Override
+           public void afterTextChanged(Editable editable) {
+           }
+       });
+
+       changedCurrency.addTextChangedListener(new TextWatcher() {
+           @Override
+           public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+           }
+
+           @Override
+           public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+           }
+
+           @Override
+           public void afterTextChanged(Editable editable) {
+
+           }
+       });
+   }
+
+   public void convertUSD(String currencyType, double userInput){     //converting all currency types to USD, then to respective selected types
+       switch(currencyType){
+           case "usd":
+               userInputUsd = userInput;
+               break;
+           case "gbp":
+               userInputUsd = userInput * 1.34;
+               break;
+           case "sgd":
+               userInputUsd = userInput * 0.74;
+               break;
+           case "thb":
+               userInputUsd = userInput * 0.031;
+               break;
+           case "myr":
+               userInputUsd = userInput * 0.25;
+               break;
+           case "jpy":
+               userInputUsd = userInput * 0.0088;
+               break;
+           case "eur":
+               userInputUsd = userInput * 1.18;
+               break;
+           case "aud":
+               userInputUsd = userInput * 0.75;
+               break;
+           case "idr":
+               userInputUsd = userInput * 0.000074;
+               break;
+           case "nzd":
+               userInputUsd = userInput * 0.69;
+               break;
+           case "inr":
+               userInputUsd = userInput * 0.016;
+               break;
+           case "hkd":
+               userInputUsd = userInput * 0.13;
+               break;
+           case "":
+           break;
+       }
+   }
+
+   public void convertActual (String currencyType, double userInputUsd){
+        switch(currencyType){
+            case "usd":
+                convertedResult = userInputUsd;
+                break;
+            case "gbp":
+                convertedResult = userInputUsd * 0.75;
+                break;
+            case "sgd":
+                convertedResult = userInputUsd * 1.35;
+                break;
+            case "thb":
+                convertedResult = userInputUsd * 32.62;
+                break;
+            case "myr":
+                convertedResult = userInputUsd * 4.07;
+                break;
+            case "jpy":
+                convertedResult = userInputUsd * 113.35;
+                break;
+            case "eur":
+                convertedResult = userInputUsd * 0.85;
+                break;
+            case "aud":
+                convertedResult = userInputUsd * 1.33;
+                break;
+            case "idr":
+                convertedResult = userInputUsd * 13549;
+                break;
+            case "nzd":
+                convertedResult = userInputUsd * 1.45;
+                break;
+            case "inr":
+                convertedResult = userInputUsd * 64.39;
+                break;
+            case "hkd":
+                convertedResult = userInputUsd * 7.81;
+                break;
+            case "":
+                break;
+        }
+   }
 }
--- app/src/main/java/example/assignmentone/cp3406/converter/CurrencyOptionsActivity.java
+++ app/src/main/java/example/assignmentone/cp3406/converter/CurrencyOptionsActivity.java
@@ -1,24 +1,184 @@
 package example.assignmentone.cp3406.converter;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class CurrencyOptionsActivity extends AppCompatActivity {
+
+    Boolean currencyOptionString;
+    ImageButton audButton; ImageButton cadButton; ImageButton chfButton;
+    ImageButton cnyButton; ImageButton eurButton; ImageButton gbpButton;
+    ImageButton hkdButton; ImageButton idrButton; ImageButton inrButton;
+    ImageButton jpyButton; ImageButton myrButton; ImageButton nzdButton;
+    ImageButton sgdButton; ImageButton thbButton; ImageButton usdButton;
+    Button returnButton;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency);
+        audButton = findViewById(R.id.audButton);
+
+        eurButton = findViewById(R.id.eurButton);
+        gbpButton = findViewById(R.id.gbpButton);
+        hkdButton = findViewById(R.id.hkdButton);
+        idrButton = findViewById(R.id.idrButton);
+        inrButton = findViewById(R.id.inrButton);
+        jpyButton = findViewById(R.id.jpyButton);
+        myrButton = findViewById(R.id.myrButton);
+        nzdButton = findViewById(R.id.nzdButton);
+        sgdButton = findViewById(R.id.sgdButton);
+        thbButton = findViewById(R.id.thbButton);
+        usdButton = findViewById(R.id.usdButton);
+        returnButton = findViewById(R.id.returnButton);
+        preferences = getSharedPreferences("Settings", MODE_PRIVATE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currencyOptionString = preferences.getBoolean("currencyBoolean", true );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        currencyOptionString = preferences.getBoolean("currencyBoolean", true );
     }
 
     public void onButtonPress(View view){
-        switch(view.getId()){
-            case (R.id.homeButton):
-                Intent menuAcvitity = new Intent(this, MainActivity.class);
+        Intent menuActivity = new Intent(this, ConvertCurrency.class);
 
+        switch(view.getId()){
+            case (R.id.audButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "aud").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "aud").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.eurButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "eur").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "eur").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.gbpButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "gbp").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "gbp").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.hkdButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "hkd").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "hkd").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.idrButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "idr").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "idr").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.inrButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "inr").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "inr").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.jpyButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "jpy").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "jpy").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.myrButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "myr").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "myr").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.nzdButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "nzd").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "nzd").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.sgdButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "sgd").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "sgd").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.thbButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "thb").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "thb").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.usdButton):
+                if(currencyOptionString){
+                    preferences.edit().putString("Option1", "usd").apply();
+                }
+                else{
+                    preferences.edit().putString("Option2", "usd").apply();
+                }
+                startActivity(menuActivity);
+                break;
+
+            case (R.id.returnButton):
+                startActivity(menuActivity);
+                break;
         }
     }
-
 }
